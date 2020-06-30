0ddfbb8a68c537f1fee21567a59f5c35f71fdcf4
QAMilestoneAcademy <48306511+QAMilestoneAcademy@users.noreply.github.com>
2020-06-30 16:13:41 +0400

    Sample

---
+++ src/test/java/qamilestone/FirstTest.java
@@ -0,0 +1,22 @@
+package qamilestone;
+
+import org.testng.annotations.Test;
+
+public class FirstTest {
+    @Test
+    public void login()
+    {
+        System.out.println("I am logging");
+    }
+    @Test
+    public void working()
+    {
+        System.out.println("I am working");
+    }
+    @Test
+    public void logout()
+    {
+        System.out.println("I am logging out");
+    }
+
+}
